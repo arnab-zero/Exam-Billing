e2e09b63650ee48c176a07d6e8fda54bc64de873
th_abir303 <bsse1321@iit.du.ac.bd>
2024-05-14 15:57:49 +0600

    Body and footer added

--- src/pages/ExamBillForm.jsx
+++ src/pages/ExamBillForm.jsx
@@ -1,11 +1,13 @@
 import BillBody from "../components/BillBody";
 import BillHeading from "../components/BillHeading";
+import BillFooter from "../components/BiilFooter"
 
 const ExamBillForm = () => {
   return (
     <div>
       <BillHeading />
       <BillBody/>
+      <BillFooter/>
     </div>
   );
 };
